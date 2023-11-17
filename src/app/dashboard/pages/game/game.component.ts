d68ee9299e4d72ce91e8c2238ad2b642c0102f6b
Franco Rodriguez <Franco.d.r1992@gmail.com>
2023-11-17 17:26:36 -0300

    boardgame-component in progess

--- src/app/dashboard/pages/game/game.component.ts
+++ src/app/dashboard/pages/game/game.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResponsiveCarouselOptions } from '../../interfaces';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  responsiveOptions: ResponsiveCarouselOptions[] | undefined;
+  news: any = [
+    { bgImg:'../../../../assets/boardgame carousel imgs/boargame img1.jpeg'},
+    { bgImg:'../../../../assets/boardgame carousel imgs/boargame img2.jpeg'},
+    { bgImg:'../../../../assets/boardgame carousel imgs/boargame img3.jpeg'}
+  ];
 
+  ngOnInit(): void {
+      this.responsiveOptions = [
+        {
+            breakpoint: '1199px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '991px',
+            numVisible: 1,
+            numScroll: 1
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
+    }
 }
+
